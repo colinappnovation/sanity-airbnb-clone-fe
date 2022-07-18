32565860c3c851d135a2f263a540f21422fb9a20
Colin McClure <colin@appnovation.com>
2022-07-18 18:12:47 +0100

    beginings of hero

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
+import { PortableText } from "@portabletext/react";
+import client from "../lib/client";
+import { getHero } from "../lib/queries";
+import Image from "next/image";
+import { urlFor } from "../utils/image";
+import Link from "next/link";
+
 // index.js
-const Index = () => {
+const Index = ({ hero }) => {
   return (
-    <div>
-      <p>Hello world!</p>
+    <div className="prose">
+      <div>
+        <Image
+          src={urlFor(hero.image, client).width(800).height(400).url()}
+          width={800}
+          height={400}
+          layout="responsive"
+        />
+        <h1>{hero.title}</h1>
+        <h2>{hero.subTitle}</h2>
+        {hero.text && <PortableText value={hero.text} />}
+        {hero.buttons && (<div>
+          {hero.buttons.map(b => {
+            return <Link href={b.url}><a>{b.linkText}</a></Link>
+          })}
+        </div>)}
+      </div>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  const hero = await client.fetch(getHero);
+  return {
+    props: {
+      hero: hero[0] || null,
+    },
+  }
+}
 
 export default Index;
